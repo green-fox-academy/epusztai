4521a7320694635ed91f4ced7b9e24608da24318
Edit Pusztai <e5pusztai@gmail.com>
2018-10-17 11:01:48 +0200

    Working with IntelliJ

---
+++ week-03/Expressions/src/HelloOthers.java
@@ -0,0 +1,13 @@
+public class HelloOthers {
+    public static void main(String[] args) {
+        // Greet 3 of your classmates with this program, in three separate lines
+        // like:
+        //
+        // Hello, Esther!
+        // Hello, Mary!
+        // Hello, Joe!
+        System.out.println("Hello, Erika!");
+        System.out.println("Hello, Viola!");
+        System.out.println("Hello, Eszter!");
+    }
+}
