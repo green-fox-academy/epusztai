38136bb34ca9e332376f00ab0c8c6e36142662e2
Edit Pusztai <e5pusztai@gmail.com>
2018-10-19 09:28:29 +0200

    Exercise 14.

--- week-03/day-3/Expressions2/src/HelloUser.java
+++ week-03/day-3/Expressions2/src/HelloUser.java
@@ -5,18 +5,13 @@ public class HelloUser {
 
         public static void main(String[] args) {
             Scanner scanner = new Scanner(System.in);
-
+            System.out.println("Please enter your name:");
             // The program stops and waits for user input and to press enter
             String userInput1 = scanner.nextLine();
 
             // It prints the whole line that was given by the user
-            System.out.println(userInput1);
+            System.out.println("Hello, " + userInput1 + "!");
 
-            // The program stops and waits for user input that is an integer and to press enter
-            int userInput2 = scanner.nextInt();
-
-            // It prints the integer
-            System.out.println(userInput2);
         }
     }
-}
+
