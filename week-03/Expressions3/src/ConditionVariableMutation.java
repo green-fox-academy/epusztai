cab5530689da3808eb4133331ce5f9092453611d
Edit Pusztai <e5pusztai@gmail.com>
2018-10-19 14:26:27 +0200

    Exercise 22

---
+++ week-03/Expressions3/src/ConditionVariableMutation.java
@@ -0,0 +1,11 @@
+public class ConditionVariableMutation {
+
+    public static void main(String[] args) {
+        double a = 24;
+        int out = 0;
+        if (a % 2 == 0) {
+            out++;
+        }
+        System.out.println(out);
+    }
+}
