8bcb938aa00131a7af8da0befdd20d3d1f2f6921
Edit Pusztai <e5pusztai@gmail.com>
2018-10-25 09:54:45 +0200

    Functions and Arrays, Exercise 2: Greeter function.

---
+++ week-04/day-1/FunctionsAndArrays/src/Greet.java
@@ -0,0 +1,11 @@
+public class Greet {
+
+    public static void main(String[] args) {
+        String a1 ="Greenfox";
+        greet(a1);
+    }
+
+    public static void greet(String message){
+        System.out.println("Greetings dear, " + message);
+    }
+}
