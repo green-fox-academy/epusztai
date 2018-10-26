2c068daac44b2107422a59870121e08af7101246
Edit Pusztai <e5pusztai@gmail.com>
2018-10-26 21:33:16 +0200

    Functions and arrays, Exercise 6 - Print arguments

---
+++ week-04/day-1/FunctionsAndArrays/src/PrintParams.java
@@ -0,0 +1,16 @@
+public class PrintParams {
+    public static void main(String[] args) {
+        printParams ("first");
+        printParams ("first", "second");
+        printParams("first", "second", "third", "fourth");
+        printParams("first", "second", "third", "fourth", "fifth");
+        printParams("first", "second", "third", "fourth", "fifth", "sixth");
+    }
+
+    public static void printParams(String...parameters){
+        for (String p: parameters ) {
+            System.out.print(p + ", ");
+        }
+        System.out.println();
+    }
+}
