c17a685addc3774e28b9e92140ebbd4bd262a940
Edit Pusztai <e5pusztai@gmail.com>
2018-10-28 18:35:46 +0100

    Functions and arrays, Exercise 15-19: AppendA, Colors, Reverse, Sum all, and Swap elements

---
+++ week-04/day-1/FunctionsAndArrays/src/Reverse.java
@@ -0,0 +1,13 @@
+public class Reverse {
+
+    public static void main(String[] args) {
+
+        int[] aj = {3, 4, 5, 6, 7};
+        int[] reverse = new int[aj.length];
+        for (int i = 0; i < reverse.length; i++) {
+            reverse[i] = aj[aj.length - 1 - i];
+
+            System.out.println(reverse[i]);
+        }
+    }
+}
