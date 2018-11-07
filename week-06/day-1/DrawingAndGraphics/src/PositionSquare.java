1ebe0e27d96cedea23061f7067e1d072017ea65e
Edit Pusztai <e5pusztai@gmail.com>
2018-11-07 11:30:06 +0100

    Drawing and Graphics: Square positioning

---
+++ week-06/day-1/DrawingAndGraphics/src/PositionSquare.java
@@ -0,0 +1,42 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class PositionSquare {
+
+    public static void mainDraw(Graphics graphics){
+
+        Color[] colors = {Color.darkGray, Color.blue, Color.cyan};
+        int size = 50;
+
+        for (int x = 0; x < 3 ; x++) {
+            graphics.setColor(colors[x]);
+            graphics.fillRect(135, (x * 70) + 60, size,size);
+        }
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
