c37ce42d3ebade092128de7ac973556bda5ceb0e
Edit Pusztai <e5pusztai@gmail.com>
2018-11-07 11:14:17 +0100

    Drawing and Graphics: FourRectangles

---
+++ week-06/day-1/DrawingAndGraphics/src/FourRectangles.java
@@ -0,0 +1,43 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class FourRectangles {
+
+    public static void mainDraw(Graphics graphics) {
+
+        Color[] colors = {Color.black, Color.blue, Color.cyan, Color.darkGray};
+        int size = 20;
+
+        for (int x = 0; x <= 3 ; x++) {
+            graphics.setColor(colors[x]);
+            graphics.fillRect(150, (x * 50) + 90, size,size);
+            size = size + 10;
+        }
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
