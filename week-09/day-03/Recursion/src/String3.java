4e552554d8ecd75ac530a2cfbb937fc282186440
Edit Pusztai <e5pusztai@gmail.com>
2018-11-29 14:40:31 +0100

    String3 fixed

--- week-09/day-03/Recursion/src/String3.java
+++ week-09/day-03/Recursion/src/String3.java
@@ -1,12 +1,12 @@
 public class String3 {
     public static void main(String[] args) {
         String text = "Greenfox";
-        System.out.print(replaceEverySecond(text));
+        System.out.print(separateCharsByStar(text));
     }
 
-    public static String replaceEverySecond(String text) {
+    public static String separateCharsByStar(String text) {
         if (text.length() > 1) {
-            return text.charAt(0) + "*" + replaceEverySecond(text.substring(1));
+            return text.charAt(0) + "*" + separateCharsByStar(text.substring(1));
         }
         return text;
     }
